63609fe12287390c33da19e19686f5405cfff036
steverhee3 <75031054+steverhee3@users.noreply.github.com>
2021-01-12 20:15:27 +0900

    Update script.js

--- function/script.js
+++ function/script.js
@@ -71,4 +71,28 @@ function load () {
     currentActive = 4;
     
   }
+ circles.forEach((circle, idx) => {
+    if(idx < currentActive) {
+      circle.classList.add('active')
+    }
+    else {
+      circle.classList.remove('active')
+    }
+  })
+  const actives = document.querySelectorAll('.active')
+  
+  progress.style.width=(actives.length-1) / (circles.length-1)*100+'%'
+  
+  if(currentActive=== 1) {
+    prev.disabled = true
+  }
+  else if(currentActive === circles.length){
+    next.disabled= true
+    
+  }
+  else {
+    prev.disabled = false
+    next.disabled = false
+  }
+ 
 }
