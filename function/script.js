e54c54b5b514c8ba6adb4eb334af8ae7f5b225ea
steverhee3 <75031054+steverhee3@users.noreply.github.com>
2021-01-12 21:19:05 +0900

    Update script.js

--- function/script.js
+++ function/script.js
@@ -128,17 +128,14 @@ next.addEventListener('load', () => {
   
   progress.style.width=(actives.length-1) / (circles.length-1)*100+'%'
   
-  if(currentActive=== 1) {
+  if(currentActive <= 1) {
     prev.disabled = true
   }
-  else if(currentActive === circles.length){
+  else if(currentActive >= circles.length){
     next.disabled= true
     
   }
-  else {
-    prev.disabled = false
-    next.disabled = false
-  }
+  
 
 
 })
