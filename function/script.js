21173c24bd958fbef2202c020ad4612d34eaee8b
steverhee3 <75031054+steverhee3@users.noreply.github.com>
2021-01-12 18:37:13 +0900

    Update script.js

--- function/script.js
+++ function/script.js
@@ -5,20 +5,7 @@ const circles = document.querySelectorAll('.circle')
 
 
 next.addEventListener('click', () => {
-  var b = document.querySelector('p').id;
-  if (b == '1') {
-    let currentActive = 1;  
-  }
-  else if (b == '2') {
-    let currentActive = 2;
-  }
-  else if (b == '3') {
-    let currentActive = 3;
-  }
-  else if (b == '4') {
-    let currentActive = 4;
-  }
-  
+  load()
   currentActive++
   if(currentActive > circles.length) {
     currentActive= circles.length
@@ -27,19 +14,7 @@ next.addEventListener('click', () => {
 })
 
 prev.addEventListener('click', () => {
-  var b = document.querySelector('p').id;
-  if (b == '1') {
-    let currentActive = 1;  
-  }
-  else if (b == '2') {
-    let currentActive = 2;
-  }
-  else if (b == '3') {
-    let currentActive = 3;
-  }
-  else if (b == '4') {
-    let currentActive = 4;
-  }
+  load()
   currentActive--
   if(currentActive < 1) {
     currentActive= 1
@@ -76,5 +51,19 @@ function update() {
 
 
 
-
+function load () {
+  var b = document.querySelector('p').id;
+  if (b == '1') {
+    let currentActive = 1;  
+  }
+  else if (b == '2') {
+    let currentActive = 2;
+  }
+  else if (b == '3') {
+    let currentActive = 3;
+  }
+  else if (b == '4') {
+    let currentActive = 4;
+  }
+}
 
