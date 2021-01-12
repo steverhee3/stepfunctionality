f20530bc358897816211b1cc9f80d5e0525513ba
steverhee3 <75031054+steverhee3@users.noreply.github.com>
2021-01-12 21:20:22 +0900

    Update script.js

--- function/script.js
+++ function/script.js
@@ -40,16 +40,12 @@ function update() {
   
   progress.style.width=(actives.length-1) / (circles.length-1)*100+'%'
   
-  if(currentActive=== 1) {
+  if(currentActive <= 1) {
     prev.disabled = true
   }
-  else if(currentActive === circles.length){
+  else if(currentActive >= circles.length){
     next.disabled= true
     
-  }
-  else {
-    prev.disabled = false
-    next.disabled = false
   }
 }
 function load () {
@@ -83,16 +79,12 @@ function load () {
   
   progress.style.width=(actives.length-1) / (circles.length-1)*100+'%'
   
-  if(currentActive=== 1) {
+  if(currentActive <= 1) {
     prev.disabled = true
   }
-  else if(currentActive === circles.length){
+  else if(currentActive >= circles.length){
     next.disabled= true
     
-  }
-  else {
-    prev.disabled = false
-    next.disabled = false
   }
  
 }
